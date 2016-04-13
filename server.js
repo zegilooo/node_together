3abb03cc1d43bc51eeab4397e662d5e7ba9bed92
Gilles Eschenbrenner <zegilooo@gmail.com>
2016-04-13 16:56:10 +0200

    add pickOne and Motivations packages

--- server.js
+++ server.js
@@ -4,5 +4,8 @@ var host = '0.0.0.0';
 var express = require('express');
 var app = express();
 
+var rules = require('./app/index.js');
+app.use(rules);
+
 app.listen(port, host);
 console.log('it work\'s ! visit http://localhost:'+port);
