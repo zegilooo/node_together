acd6318228dc7fbad4e0f2646daadf168796b0f7
Gilles Eschenbrenner <zegilooo@gmail.com>
2016-04-13 23:38:31 +0200

    using handlebars

--- app/index.js
+++ app/index.js
@@ -1,14 +1,29 @@
 var express = require('express');
+var exphbs = require('express-handlebars');
 const motivations = require('motivations');
 const pickOne = require('pick-one');
+const catImages = [1, 2, 3, 4, 5, 6];
+
 var app = express();
 
-app.get("/", function(request,response){
-	response.send(pickOne(motivations));
+app.engine('handlebars', exphbs({
+	defaultLayout: 'main',
+	layoutsDir: './app/views/layouts'
+}));
+app.set('views', './app/views')
+app.set('view engine', 'handlebars');
+
+app.get("/", function(request, response) {
+	var motivation = pickOne(motivations);
+	var image = pickOne(catImages);
+	response.render('motivations', {
+		motivation: motivation,
+		image:image
+	});
 });
 
-app.get("/about", function(request,response){
+app.get("/about", function(request, response) {
 	response.send("This is about what ?");
 });
 
-module.exports =  app;
+module.exports = app;
